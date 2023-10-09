7167c4e464f4bd1a596f78bab40a24295ab6a63f
GuanJms <45347032+GuanJms@users.noreply.github.com>
2023-10-09 02:15:01 -0400

    Update
    update

--- Proeject/test_mongoVM.py
+++
@@ -1,8 +0,0 @@
-from pymongo import MongoClient
-
-client = MongoClient("mongodb://433-22.csse.rose-hulman.edu:27017")
-
-db = client['bookstore']
-res = db.authors.find()
-for data in res:
-    print(data)
